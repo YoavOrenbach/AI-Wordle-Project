8dc3efac6756a189050855fe06f02cf543c20f9e
Adi Rabinovitz <adiwwww@gmail.com>
2022-07-15 18:33:34 +0300

    Starting minimax algorithm

--- Algorithms/minimax.py
+++ Algorithms/minimax.py
@@ -2,14 +2,21 @@ import util
 from Algorithms.algorithm import Algorithm
 from WordleGames.abstract_wordle_logic import AbstractWordleLogic
 
+# Added
+from game_visible_state import GameVisibleState
+
 
 class Minimax(Algorithm):
     def __init__(self):
         super(Minimax, self).__init__("MiniMax")
-        util.raiseNotDefined()
+        self.guess_count = 0
 
     def get_action(self, game_state, game_logic: AbstractWordleLogic):
-        util.raiseNotDefined()
+        """comments for myself:
+        game state is """
+        game_state = GameVisibleState()
+        high_score = float('-inf')
+
 
     def reset(self):
         util.raiseNotDefined()
