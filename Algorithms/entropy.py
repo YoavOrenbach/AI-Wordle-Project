1b3cad5b21d2da68b45a487d21af466303349553
Amit Keinan <amitk2612@gmail.com>
2022-07-23 10:41:08 +0300

    specific absurdle-entropy support

--- Algorithms/entropy.py
+++ Algorithms/entropy.py
@@ -5,22 +5,30 @@ from tqdm import tqdm
 
 from Algorithms.algorithm import Algorithm
 from WordleGames.abstract_wordle_logic import AbstractWordleLogic
+from WordleGames.utils import get_pattern_vanilla
 from common import Word
 from game_visible_state import GameVisibleState
 
 
 class Entropy(Algorithm):
-    opening_guesses = {"Wordle": "tares", "Yellow Wordle": "arise"}  # these words were pre-computed using the same algorithm
+    opening_guesses = {"Wordle": "tares", "Yellow Wordle": "arise",
+                       "Absurdle": "tares"}  # these words were pre-computed using the same algorithm
 
     def __init__(self):
         super(Entropy, self).__init__("Entropy")
+
+    def get_pattern(self, guess: Word, secret_word: Word, game_state: GameVisibleState, game_logic: AbstractWordleLogic):
+        if game_logic.name in ["Absurdle"]:
+            return get_pattern_vanilla(guess, secret_word)
+        else:
+            return game_logic.get_pattern(guess, secret_word, game_state)
 
     def get_pattern_probs(self, guess: Word, game_state: GameVisibleState, game_logic: AbstractWordleLogic) -> Dict[
         tuple, float]:
         pattern_counts = {pattern: 0 for pattern in game_logic.all_patterns}
         possible_secret_words = game_logic.cur_possible_words
         for secret_word in possible_secret_words:
-            pattern_counts[tuple(game_logic.get_pattern(guess, secret_word, game_state))] += 1
+            pattern_counts[tuple(self.get_pattern(guess, secret_word, game_state, game_logic))] += 1
 
         possible_words_num = len(possible_secret_words)
         pattern_probs = {k: v / possible_words_num for k, v in pattern_counts.items()}
